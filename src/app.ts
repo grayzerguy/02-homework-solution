c8acd151f2da7c4e5a53907d3f70f9b328d0de04
grayzerguy <58952605+grayzerguy@users.noreply.github.com>
2025-07-03 08:00:57 +0300

    Delete src directory

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { findPrimes } from './find-primes ';
-
-const result = findPrimes(1, 40);
-
- result.primes.forEach(p => console.log(p));
-//  console.log(result);
-// console.log(result.primes)
